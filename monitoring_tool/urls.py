8511d58f8a6d410dba7f4c5c03a45a56dee49b19
johnamdickson <johnamdickson@me.com>
2023-10-10 19:28:43 +0000

    refactor: amend view to EmissionList from Emission

--- monitoring_tool/urls.py
+++ monitoring_tool/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.Emission.as_view(), name="home"),
+    path("", views.EmissionList.as_view(), name="home"),
 ]
