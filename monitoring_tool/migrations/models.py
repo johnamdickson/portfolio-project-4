1464fd8707b82e2e3c19ad75aad54ff58811ff85
johnamdickson <johnamdickson@me.com>
2023-10-10 07:38:17 +0000

    build: add models.py and link to views.py create models.py file and first class of Emission with corresponding view class in views.py

---
+++ monitoring_tool/migrations/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Emission(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    location = models.CharField(max_length=50, unique=False)
+    slug = models.SlugField(max_length=200, unique=True)
+    username = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="monitoring_tool-posts"
+    )
+    description = models.TextField(blank=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField() 
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    next_check_due = models.DateField(auto_now=False)
+    current_check_due = models.DateField(auto_now=False)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
--- monitoring_tool/views.py
+++ monitoring_tool/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic, View
+from django.http import HttpResponseRedirect
+from .models import Emission
 
-# Create your views here.
+
+class EmissionList(generic.ListView):
+    model = Emission
+    queryset = Emission.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
+    paginate_by = 6
