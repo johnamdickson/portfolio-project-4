47e0462572337896f77ee7061a96a557a1e86ed4
johnamdickson <johnamdickson@me.com>
2023-10-11 09:31:05 +0000

    build: register Emission in admin.py

--- monitoring_tool/admin.py
+++ monitoring_tool/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Emission
 
-# Register your models here.
+
+@admin.register(Emission)
+class EmissionAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'description']
+    summernote_fields = ('description',)
