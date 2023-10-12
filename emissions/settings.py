0eb5b708490e10331b93ce12490d0fd5ac18de8b
johnamdickson <johnamdickson@me.com>
2023-10-12 14:18:53 +0000

    build: add templates dir to templates in settings

--- emissions/settings.py
+++ emissions/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'emissions.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
