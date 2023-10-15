90bc0333ae668c766b674f49df5574af80172338
johnamdickson <johnamdickson@me.com>
2023-10-15 09:56:45 +0000

    build: add urls to monitoring tool add emission and emission checks urls to check site navigation

--- monitoring_tool/urls.py
+++ monitoring_tool/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.EmissionList.as_view(), name="home"),
+    path("emissions/", views.Emissions.as_view(), name="emissions"),
+    path("emissions_checks/", views.EmissionChecks.as_view(), name="emission_checks"),
 ]
--- monitoring_tool/views.py
+++ monitoring_tool/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
-from .models import Emission
+from .models import Emission, EmissionCheck
 
 
 class EmissionList(generic.ListView):
@@ -9,3 +9,18 @@ class EmissionList(generic.ListView):
     queryset = Emission.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
     paginate_by = 6
+
+
+class Emissions(generic.ListView):
+    model = Emission
+    queryset = Emission.objects.filter(status=1).order_by("-created_on")
+    template_name = "emission.html"
+    paginate_by = 6
+
+
+class EmissionChecks(generic.ListView):
+    model = EmissionCheck
+    queryset = EmissionCheck.objects.filter(status=1).order_by("-date_checked")
+    template_name = "emission_checks.html"
+    paginate_by = 6
+    
