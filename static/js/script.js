712ded6e4dbb1fd255144027d5adf9a003447b96
johnamdickson <johnamdickson@me.com>
2023-10-15 19:58:10 +0000

    build: add script.js file create first function for dismissing messages after timer

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+
+// dismiss alerts after 2.5 seconds
+setTimeout(function () {
+    let messages = document.getElementById('msg');
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 2500);
