cebcb37ff4e2c94ee2945bcfa4af95a33127952d
johnamdickson <johnamdickson@me.com>
2023-10-18 16:16:35 +0000

    build: add constants file for model choices.

---
+++ constants.py
@@ -0,0 +1,13 @@
+STATUS = ((0, "Draft"), (1, "Approved"))
+
+EMISSION_TYPES = (
+    (1, ("Weep")),
+    (2, ("Seep")),
+    (3, ("Fugitive Emission")),
+)
+
+CHECK_STATUS = (
+    (1, ("None Detected")),
+    (2, ("Below Minimum Threshold")),
+    (3, ("Above Minimum Threshold")),
+)
