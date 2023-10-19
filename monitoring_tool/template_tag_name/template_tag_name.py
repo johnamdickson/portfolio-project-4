3b2f3053a0ea307f5877e485b8c3b855a979b8ad
johnamdickson <johnamdickson@me.com>
2023-10-19 08:39:20 +0000

    chore: add credit in template_tag file

--- monitoring_tool/template_tag_name/template_tag_name.py
+++ monitoring_tool/template_tag_name/template_tag_name.py
@@ -1,3 +1,7 @@
+# code for user group template from stack overflow and django documentation:
+# https://stackoverflow.com/questions/34571880/how-to-check-in-template-if-user-belongs-to-a-group
+# https://docs.djangoproject.com/en/dev/howto/custom-template-tags/
+
 from django import template
 
 register = template.Library()
