193b6ab9d41436524c18e9d28c9272df6428b562
johnamdickson <johnamdickson@me.com>
2023-10-27 23:38:53 +0000

    feat: add widget to emission form for date type

--- monitoring_tool/forms.py
+++ monitoring_tool/forms.py
@@ -2,6 +2,10 @@ from .models import Emission
 from django import forms
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class EmissionSubmissionForm(forms.ModelForm):
     class Meta:
         model = Emission
@@ -9,3 +13,9 @@ class EmissionSubmissionForm(forms.ModelForm):
                   'description', 'latitude', 'longitude',
                   'next_check_due', 'current_check_due',
                   'status', 'type')
+# solution for selecting form fields as date type from stack overflow:
+# https://stackoverflow.com/questions/22846048/django-form-as-p-datefield-not-showing-input-type-as-date
+        widgets = {
+            'next_check_due': DateInput(),
+            'current_check_due': DateInput(),
+        }
