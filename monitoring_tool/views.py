2fbea9cc50212adb7699297f4c44b8ec01533633
johnamdickson <johnamdickson@me.com>
2023-10-28 20:34:13 +0000

    feat: add a success message to add emissions
    on valid form saving, message instantiated informing user

--- monitoring_tool/views.py
+++ monitoring_tool/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.contrib.auth.models import User
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .models import Emission, EmissionCheck
 from .forms import EmissionSubmissionForm
@@ -52,14 +53,22 @@ class EmissionChecks(generic.ListView):
 def addEmission(request):
     form = EmissionSubmissionForm()
     if request.method == 'POST':
-        # issue uploading image to DB from html form. Solution from Stack Overflow:
+        # issue uploading image to DB from html form. 
+        # Solution from Stack Overflow:
         # https://stackoverflow.com/questions/45912825/image-upload-field-works-in-django-admin-but-not-working-in-template
         form = EmissionSubmissionForm(request.POST, request.FILES)
-    if form.is_valid():
-        form.instance.username = User.objects.get(username=request.user)
-        form.instance.status = 0
-        form.save()
-        return HttpResponseRedirect(reverse('emissions'))
-        # redirect('home')
+        if form.is_valid():
+            print(request.FILES)
+            form.instance.username = User.objects.get(username=request.user)
+            form.instance.status = 0
+            form.save()
+            messages.success(
+                request,
+                f"Emission {form.instance.title} successfully created!"
+                )
+            return HttpResponseRedirect(reverse('emissions'))
+        else:
+            messages.error(request, "Error")
     context = {'form': form}
     return render(request, 'add-emission.html', context)
+
