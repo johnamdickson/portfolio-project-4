b4f923921e4bd842d6759ea696462c73a5e4ccd3
johnamdickson <johnamdickson@me.com>
2023-10-29 03:39:59 +0000

    refactor: line overflow and whitespace removal

--- monitoring_tool/forms.py
+++ monitoring_tool/forms.py
@@ -7,15 +7,15 @@ class DateInput(forms.DateInput):
 
 
 class EmissionSubmissionForm(forms.ModelForm):
-  
+
     class Meta:
         model = Emission
         fields = ('title', 'location', 'emission_image',
                   'description', 'latitude', 'longitude',
                   'next_check_due', 'current_check_due',
                   'type')
-    
-# solution for selecting form fields as date type and for text area rows from stack overflow:
+# solution for selecting form fields as date type and
+# for text area rows from stack overflow:
 # https://stackoverflow.com/questions/22846048/django-form-as-p-datefield-not-showing-input-type-as-date
 # https://stackoverflow.com/questions/6536373/how-can-i-set-the-size-of-rows-columns-in-textfield-in-django-models
         widgets = {
