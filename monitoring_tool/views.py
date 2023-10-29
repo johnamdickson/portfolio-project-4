f213333df1a6119dbba9fd72f707e8e0397e194e
johnamdickson <johnamdickson@me.com>
2023-10-29 03:39:03 +0000

    feat: add conditional logic on form to check image
    check file extensions tuple of image types

--- monitoring_tool/views.py
+++ monitoring_tool/views.py
@@ -30,7 +30,6 @@ class Emissions(View):
         description = emission.description
         image_url = emission.emission_image.url
         location = emission.location
-
         return render(
             request,
             'emission_detail.html',
@@ -50,25 +49,45 @@ class EmissionChecks(generic.ListView):
     paginate_by = 6
 
 
+
 def addEmission(request):
     form = EmissionSubmissionForm()
     if request.method == 'POST':
-        # issue uploading image to DB from html form. 
-        # Solution from Stack Overflow:
-        # https://stackoverflow.com/questions/45912825/image-upload-field-works-in-django-admin-but-not-working-in-template
-        form = EmissionSubmissionForm(request.POST, request.FILES)
-        if form.is_valid():
-            print(request.FILES)
-            form.instance.username = User.objects.get(username=request.user)
-            form.instance.status = 0
-            form.save()
-            messages.success(
-                request,
-                f"Emission {form.instance.title} successfully created!"
-                )
-            return HttpResponseRedirect(reverse('emissions'))
+        # check if user has permissions to add emissions ie emission admin.
+        if request.user.has_perm('monitoring_tool.add_emission'):
+            # issue uploading image to DB from html form.
+            # Solution from Stack Overflow:
+            # https://stackoverflow.com/questions/45912825/image-upload-field-works-in-django-admin-but-not-working-in-template
+            form = EmissionSubmissionForm(request.POST, request.FILES)
+            # access form image name from request.FILES and use conditional code to proceed
+            # or generate error message.
+            form_images = request.FILES.getlist('emission_image', None)
+            form_image = form_images[0]
+            if str(form_image).endswith(('.jpg', '.jpeg', '.png', '.tiff', '.bmp')):
+                if form.is_valid():
+                    form.instance.username = User.objects.get(
+                                         username=request.user)
+                    form.instance.status = 0
+                    form.save()
+                    messages.success(
+                        request,
+                        f"Emission {form.instance.title} successfully created!"
+                        )
+                    return HttpResponseRedirect(reverse('emissions'))
+                else:
+                    error = []
+                    for field in form:
+                        if field is not None:
+                            error += field.errors
+                    messages.error(request, error)
+            else:
+                messages.error(request, "Incorrect image format. Please upload jpg, jpeg, png, tiff or bmp")
+
         else:
-            messages.error(request, "Error")
+            messages.warning(
+                request,
+                f"You do not have the necessary permissions "
+                "to create a new emission.")
     context = {'form': form}
     return render(request, 'add-emission.html', context)
 
