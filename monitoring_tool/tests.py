8b28b3f16a18741460993168659a47845489f8a8
johnamdickson <johnamdickson@me.com>
2023-11-25 21:22:59 +0000

    build: add test class and testing db

--- monitoring_tool/tests.py
+++ monitoring_tool/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from monitoring_tool.views import EmissionList
+
 
 # Create your tests here.
+
+
+class EmissionList(TestCase):
+    
+     def test_false_is_false(self):
+        print("Method: test_false_is_false.")
+        self.assertFalse(False)
