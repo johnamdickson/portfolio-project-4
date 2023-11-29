a41b92e14de73f5090b9af73d46299f418289d5c
johnamdickson <johnamdickson@me.com>
2023-11-29 00:40:06 +0000

    test: add tests for errorCountdown timer
    confirm timer span in DOM returns 5 and 0 sec for 5 and 10 sec counts

--- static/js/test-js/script.test.js
+++ static/js/test-js/script.test.js
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 
+
 (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
     ({key: "AIzaSyDFRRlo1ePBYrJnuCMcVFvqRJa_9UQMa4I", v: "weekly"});
 
@@ -12,15 +13,24 @@ beforeAll(() => {
     let fileContents = fs.readFileSync("templates/403.html", "utf-8");
     document.write(fileContents);
     document.close();
+    // use of fake timers to test errorCountdown function:
+    // https://jestjs.io/docs/next/timer-mocks
+    jest.useFakeTimers();
 });
 
 
 describe("DOM tests", () => {
-    test("Expects timer to be set at 10 seconds", () => {
-        errorCountdown();
+    test("Expects timer to be set at 10 seconds on page load", () => {
         expect(document.getElementById("error-timer")
             .innerHTML).toEqual("10");
     });
+    test("Expects timer to be set at 0 seconds after 10 seconds", async () => {
+        errorCountdown();
+        jest.advanceTimersByTime(10000);
+        console.log(document.getElementById("error-timer").innerText)
+        expect(document.getElementById("error-timer")
+            .innerText).toEqual("0");
+    })
     test("h2 should exist", () => {
         expect(document.getElementsByTagName("h2").length).toBe(1);
     });
