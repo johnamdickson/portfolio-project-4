60eeb0fef35b6f5c79a197d184fad380038da2e5
johnamdickson <johnamdickson@me.com>
2023-12-02 19:47:25 +0000

    test: update with FirstMonday class

--- monitoring_tool/tests.py
+++ monitoring_tool/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client, SimpleTestCase
 from django.shortcuts import render, get_object_or_404, reverse
 from django.contrib.auth.models import User, Group, Permission
-from .views import first_monday_current_month, first_monday_next_month
+from .views import FirstMonday
 import datetime
 from freezegun import freeze_time
 # freezegun info here: https://github.com/spulec/freezegun
@@ -212,9 +212,9 @@ class TestEmissionCalls(TestCase):
 
          
 class FirstMonday(TestCase):
-
-   first_monday_of_this_month = first_monday_current_month()
-   first_monday_of_next_month = first_monday_next_month()
+   first_monday = FirstMonday()
+   first_monday_of_this_month = first_monday.calculate_current_check_due()
+   first_monday_of_next_month = first_monday.calculate_next_check_due()
 
    def test_return_datetime(self):
       """
@@ -223,35 +223,35 @@ class FirstMonday(TestCase):
       self.assertEqual(type(self.first_monday_of_this_month), datetime.datetime)
       self.assertEqual(type(self.first_monday_of_next_month), datetime.datetime)
   
-   @freeze_time("2012-04-30")
+   @freeze_time("1978-04-30")
    def test_first_monday_this_month(self):
       """
       Use freeze gun to set datetime to 30/04/1978. This enables 
       assertEqual to check against known first Monday of that particular
       month which was 02/04/1978.
       """
-      first_monday_april_78 = first_monday_current_month()
+      first_monday_april_78 = self.first_monday.calculate_current_check_due()
       print(
          self._testMethodName.upper(), 
          "\nFirst monday of April 1978 was: ",
          first_monday_april_78, '\n'
          )  
-      self.assertEqual (first_monday_april_78, datetime.datetime(2012, 4, 2))
+      self.assertEqual (first_monday_april_78, datetime.datetime(1978, 4, 3))
 
 
-   @freeze_time("2012-04-30")
+   @freeze_time("1978-04-30")
    def test_first_monday_next_month(self):
       """
       Use freeze gun to set datetime to 30/04/1978. This enables 
       assertEqual to check against known first Monday of the following
       month which was 07/05/1978.
       """
-      first_monday_may_78 = first_monday_next_month()
+      first_monday_may_78 = self.first_monday.calculate_next_check_due()
       print(
          self._testMethodName.upper(), 
          "\nFirst monday of May 1978 was: ",
          first_monday_may_78, '\n'
          )  
-      self.assertEqual (first_monday_may_78, datetime.datetime(2012, 5, 7))
+      self.assertEqual (first_monday_may_78, datetime.datetime(1978, 5, 1))
 
 
